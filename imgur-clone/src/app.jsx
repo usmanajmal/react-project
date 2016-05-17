2ae6bc10a7270959d2cf3e2029f05f200bf1f735
Usman Ajmal <syedusmanajmal@gmail.com>
2016-05-17 16:01:47 -0700

    Imgur Clone - Added basic support for React Router
    Change-Id: Ie1ecb64973c240532664fb2f1b9944efe003f744

--- imgur-clone/src/app.jsx
+++ imgur-clone/src/app.jsx
@@ -1,13 +1,41 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router')
+// Router object which help deciding what contents to show on page
+var Router = ReactRouter.Router;
+// Route takes care of how to configure router
+var Route = ReactRouter.Route
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      {this.props.children}
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
-ReactDOM.render(element, document.querySelector('.container'));
+var Child1 = React.createClass({
+	render: function() {
+		return <h1>I am 1</h1>
+	}
+});
+
+
+var Child2 = React.createClass({
+	render: function() {
+		return <h1>I am 2</h1>
+	}
+});
+
+var routes = (
+	<Router>
+		<Route path="/" component={Hello}>
+			<Route path="1" component={Child1} />
+			<Route path="2" component={Child2} />
+		</Route>
+	</Router>
+);
+
+
+
+ReactDOM.render(routes, document.querySelector('.container'));
