baf3299d536020ad9d180ba69fc6f792dad6d37d
Usman Ajmal <syedusmanajmal@gmail.com>
2016-05-25 23:41:52 -0700

    Start creating topic-list component
    Change-Id: I05f39b6acda0da8b6e9ac1bcd0f596b52480841e
    Signed-off-by: Usman Ajmal <syedusmanajmal@gmail.com>

--- imgur-clone/src/utils/api.jsx
+++ imgur-clone/src/utils/api.jsx
@@ -3,13 +3,16 @@ var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = '7d0cc13ae1d4d57';
 
-module.exports = {
+module.exports = window.api = {
  	get: function(url) {
  		// Fetch: URL and configuration option in arguments
  		return fetch(rootUrl + url, {
  			headers: {
  				'Authorization': 'Client-ID ' + apiKey
  			}
+ 		})
+ 		.then(function (response)  {
+ 			return response.json()
  		});
  	}
 }
